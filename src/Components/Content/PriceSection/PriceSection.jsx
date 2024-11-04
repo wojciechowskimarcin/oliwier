ec4ecdd721fe2f828a638ee2db31b0e0059eb489
Marcin <mwojciechowski@zsp5kozuchow.pl>
2024-11-04 08:50:51 +0100

    Modyfikacja API Crypto wraz z dodaniem multizapytania

--- src/Components/Content/PriceSection/PriceSection.jsx
+++ src/Components/Content/PriceSection/PriceSection.jsx
@@ -9,16 +9,16 @@ const PriceSection = () => {
     useEffect(() => {
         const fetchData = async () => {
             try{
-                const data = await fetch("https://min-api.cryptocompare.com/data/price?fsym=PLN&tsyms=BT,ETH,LTH,DOG&api_key=b5b71358c0a1de505b395c192ccb04645d8076288c16802d46e95a5f1dc98684");
+                const data = await fetch("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTH,DOG&tsyms=PLN&api_key=b5b71358c0a1de505b395c192ccb04645d8076288c16802d46e95a5f1dc98684");
                 if (!data.ok){
                     throw new Error("Problem z pobraniem danych");
                 }
                 else{
-                    console.log(data);
+                    //console.log(data);
                 }
                 const result = await data.json();
                 setDane(result);
-                console.log(dane);
+                //console.log(dane.BTC.PLN);
 
             }
             catch(e){
@@ -39,22 +39,22 @@ const PriceSection = () => {
                 <div className={s.cryptoBlock}>
                     <div className={s.cryptoImg}></div>
                     <p>Bitcoin</p>
-                    <p>{dane.BT}</p>
+                    <p>{dane.BTC.PLN} zł</p>
                 </div>
                 <div className={s.cryptoBlock}>
                     <div className={s.cryptoImg}></div>
                     <p>DOG</p>
-                    <p>{dane.DOG}</p>
+                    <p>{dane.DOG.PLN} zł</p>
                 </div>
                 <div className={s.cryptoBlock}>
                     <div className={s.cryptoImg}></div>
                     <p>LTH</p>
-                    <p>{dane.LTH}</p>
+                    <p>{dane.LTH.PLN} zł</p>
                 </div>
                 <div className={s.cryptoBlock}>
                     <div className={s.cryptoImg}></div>
                     <p>ETH</p>
-                    <p>{dane.ETH}</p>
+                    <p>{dane.ETH.PLN} zł</p>
                 </div>
 
 
